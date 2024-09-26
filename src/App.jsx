024b471368a0a6f74a3bbf756253f431ef805f27
ishan1122 <56794677+ishan1122@users.noreply.github.com>
2024-09-26 11:45:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import LoadingScreen from './components/LoadingScreen';
+
 import GameScreen from './components/MainScreen';
 import './App.css';
 
